7bdc33504bd5ccd5338109d082c20e73faaaec12
Joel <joelw16@gmail.com>
2017-03-21 23:48:29 +0000

    GUI Update, new tab for creating a tour

--- app/src/main/java/uk/co/joelwalker/bebop_follow_me/start.java
+++ app/src/main/java/uk/co/joelwalker/bebop_follow_me/start.java
@@ -7,11 +7,16 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import org.json.JSONException;
@@ -20,25 +25,66 @@ import org.json.JSONObject;
 import uk.co.joelwalker.bebop_follow_me.server.Connection;
 import uk.co.joelwalker.bebop_follow_me.server.Constants;
 
-public class MainActivity extends AppCompatActivity implements LocationListener {
+public class start extends AppCompatActivity implements LocationListener {
+
+    private TextView connection_txt, mobile_gps;
+
+    private FrameLayout home_content, tour_content, create_content;
+
+    private FloatingActionButton gps_fab;
 
     private View mainView;
-    TextView mobile_GPS_Text, other_text;
+
     public final int MY_PERMISSIONS_REQUEST_FINE_LOCATION = 0;
+
     LocationManager locationManager;
     boolean connected = false;
 
 
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    create_content.setVisibility(View.INVISIBLE);
+                    tour_content.setVisibility(View.INVISIBLE);
+                    home_content.setVisibility(View.VISIBLE);
+                    return true;
+                case R.id.navigation_dashboard:
+                    create_content.setVisibility(View.INVISIBLE);
+                    home_content.setVisibility(View.INVISIBLE);
+                    tour_content.setVisibility(View.VISIBLE);
+                    return true;
+                case R.id.navigation_notifications:
+                    home_content.setVisibility(View.INVISIBLE);
+                    tour_content.setVisibility(View.INVISIBLE);
+                    create_content.setVisibility(View.VISIBLE);
+                    return true;
+            }
+            return false;
+        }
+
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_start);
 
-        mainView = findViewById(R.id.content_view);
+        connection_txt = (TextView) findViewById(R.id.connection_txt);
+        mobile_gps = (TextView) findViewById(R.id.mob_gps_txt);
 
-        Log.i("GPS", "INIT");
-        mobile_GPS_Text = (TextView) findViewById(R.id.mob_gps_text);
-        other_text = (TextView) findViewById(R.id.drone_gps_text);
+
+        home_content = (FrameLayout) findViewById(R.id.home_content);
+        tour_content = (FrameLayout) findViewById(R.id.tour_content);
+        create_content =(FrameLayout) findViewById(R.id.create_content);
+
+        gps_fab = (FloatingActionButton) findViewById(R.id.gps_fab);
+
+        mainView = findViewById(R.id.container);
+
 
         if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
@@ -55,12 +101,25 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
             locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
         }
+
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        connectServer();
+
+        gps_fab.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if(!connected){
+                    connectServer();
+                }else{
+                    disconnectServer();
+                }
+            }
+        });
     }
 
     @Override
@@ -105,10 +164,9 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
         } catch (JSONException e) {
             e.printStackTrace();
         }
-
+        connected = false;
         DroneServer droneServer = new DroneServer(this.getApplicationContext());
         droneServer.execute(json);
-        connected = false;
     }
 
     private void sendGPS(double longitude, double latitude, float accuracy){
@@ -127,13 +185,14 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
 
     @Override
     public void onLocationChanged(Location location) {
-        mobile_GPS_Text.setText(location.getLatitude() + " "+ location.getLongitude() + " " + location.getAccuracy());
+        mobile_gps.setText(location.getLatitude() + " "+ location.getLongitude() + " " + location.getAccuracy());
         location.getAccuracy();
         if(connected){
             sendGPS(location.getLongitude(), location.getLatitude(), location.getAccuracy());
         }
 
     }
+
 
     @Override
     public void onStatusChanged(String s, int i, Bundle bundle) {
@@ -150,7 +209,6 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
 
     }
 
-
     public class DroneServer extends AsyncTask<JSONObject, Void, String> {
 
         final String TAG = "API";
@@ -162,25 +220,24 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
 
         @Override
         protected String doInBackground(JSONObject... params) {
-
             JSONObject json = params[0];
-
             return connection.postRequest(Constants.SERVER_URL + Constants.Android_API, json);
-
         }
 
         @Override
         protected void onPostExecute(String s) {
             Log.i(TAG, "JSON: " + s);
             if(s.contains("Error")){
-                other_text.setText("Error");
+                connection_txt.setText("Status: Sever Unavailable ");
             }else{
-                other_text.setText("Connected");
+                if(connected){
+                    connection_txt.setText("Status: Connected");
+                }else{
+                    connection_txt.setText("Status: Disconnected");
+                }
             }
 
         }
 
     }
-
-
 }
